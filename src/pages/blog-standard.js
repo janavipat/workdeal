d9f59a5dd52b235f0a498a5d20101b979728824a
lanet <lanet@gmail.com>
2024-04-20 23:36:54 +0530

    blog standard page change

--- src/pages/blog-standard.js
+++ src/pages/blog-standard.js
@@ -111,29 +111,7 @@ function BlogStandardPage() {
                   );
                 })}
               </div>
-              <div
-                className="paginatation wow animate fadeInUp"
-                data-wow-delay="200ms"
-                data-wow-duration="1500ms"
-              >
-                <ul className="paginate">
-                  <li>
-                    <a href="#">Previous</a>
-                  </li>
-                  <li>
-                    <a href="#">01</a>
-                  </li>
-                  <li className="active">
-                    <a href="#">02</a>
-                  </li>
-                  <li>
-                    <a href="#">03</a>
-                  </li>
-                  <li>
-                    <a href="#">Next</a>
-                  </li>
-                </ul>
-              </div>
+              
             </div>
           </div>
         </div>
