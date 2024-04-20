d9f6d24389934fd19b60e4d031471c330cc109f4
lanet <lanet@gmail.com>
2024-04-20 10:49:22 +0530

    set header

--- src/components/header/Header1.jsx
+++ src/components/header/Header1.jsx
@@ -1,16 +1,21 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-import React, { useEffect, useReducer, useRef, useContext,useState } from "react";
+import React, {
+  useEffect,
+  useReducer,
+  useRef,
+  useContext,
+  useState,
+} from "react";
 import { auth } from "../../firebase/firebase";
 import { MyContext } from "../context";
+import axios from "axios";
 // inital state data
 const initialState = {
   activeMenu: "",
   menuOpen: false,
   scrollY: 0,
 };
-
-
 
 // usnig reducer to change logic
 function reducer(state, action) {
@@ -44,9 +49,13 @@ function Header1() {
   const [state, dispatch] = useReducer(reducer, initialState);
   const currentRoute = useRouter().pathname;
   const headerRef = useRef(null);
-  const [mobileHeader,setMobileheader]=useState(false);
-
+  const [mobileHeader, setMobileheader] = useState(false);
+  const [authentication, setAuthentication] = useState(null);
+  const [image, setImage] = useState();
+  const [typeoff, setTypeoff] = useState();
+  const [fname, setFname] = useState();
   const { serviceType, updateVariable } = useContext(MyContext);
+
   const handleInitialServices = () => {
     updateVariable({ location: "", category: "", pricerange: "", rating: "" });
   };
@@ -54,6 +63,45 @@ function Header1() {
   function handleMenu(menuName) {
     dispatch({ type: "TOGGLE", payload: menuName });
   }
+
+  useEffect(() => {
+    auth.onAuthStateChanged(function (user) {
+      if (user) {
+        setAuthentication(user);
+      } else {
+      }
+    });
+    const getData = async () => {
+      if (auth.currentUser) {
+        const useruid = auth.currentUser.uid;
+        const idtoken = await auth.currentUser.getIdToken();
+        axios
+          .post(`http://localhost:5000/get-user-data/`, {
+            uid: useruid,
+            idtoken: idtoken,
+          })
+          .then((data) => {
+            if (data.status == 200) {
+              
+              setImage(data.data.imageUrl);
+              setTypeoff(data.data.typeofacc);
+              setFname(data.data.fname);
+            } else {
+              window.alert("something went wrong");
+            }
+          })
+          .catch((error) => {
+            if (error && error.response && error.response.status == 404) {
+              window.location = "/login-google-required";
+            } else {
+              window.alert(error.message);
+            }
+          });
+      }
+    };
+
+    getData();
+  }, [authentication]);
 
   useEffect(() => {
     const burger = document.querySelector(".mobile-menu");
@@ -67,7 +115,7 @@ function Header1() {
     });
   });
 
-  // sticky header
+
   useEffect(() => {
     const header = headerRef.current;
 
@@ -85,13 +133,9 @@ function Header1() {
       window.removeEventListener("scroll", handleScroll);
     };
   }, []);
-  
+
   return (
-    <header
-      ref={headerRef}
-      // just use one header class for your project
-      className={"header-1 sticky_top"}
-    >
+    <header ref={headerRef} className={"header-1 sticky_top"}>
       <div className="header-logo">
         <Link legacyBehavior href="/">
           <a>
@@ -108,18 +152,7 @@ function Header1() {
           <div className="mobile-menu-logo">
             <Link legacyBehavior href="/">
               <a>
-                <img
-                  /*style="max-width: 230px;
-			position: absolute;
-			top: 0;
-			width: 100%;
-			height: 80%;
-			object-fit: cover;
-			object-position: 55%;"*/
-
-                  src={"assets/images/wlogo.png"}
-                  alt=""
-                />
+                <img src={"assets/images/wlogo.png"} alt="" />
               </a>
             </Link>
             <div className="remove">
@@ -127,20 +160,20 @@ function Header1() {
             </div>
           </div>
           <ul>
-            <li className="has-child active">
+            <li className="has-child active" style={{textDecoration:"none"}}>
               <Link legacyBehavior href="/">
                 <a className={currentRoute === "/" ? "active" : ""}>Home</a>
               </Link>
             </li>
             <li>
-              <Link legacyBehavior href="/about">
+              <Link legacyBehavior href="/about"  style={{textDecoration:"none"}}>
                 <a className={currentRoute === "/about" ? "active" : ""}>
                   About Us
                 </a>
               </Link>
             </li>
             <li>
-              <Link legacyBehavior href="/service">
+              <Link legacyBehavior href="/service"  style={{textDecoration:"none"}}>
                 <a
                   onClick={handleInitialServices}
                   className={currentRoute === "/service" ? "active" : ""}
@@ -150,21 +183,21 @@ function Header1() {
               </Link>
             </li>
             <li>
-              <Link legacyBehavior href="/blog-details">
-                <a className={currentRoute === "/blog-details" ? "active" : ""}>
+              <Link legacyBehavior href="/blog"  style={{textDecoration:"none"}}>
+                <a className={currentRoute === "/blog" ? "active" : ""}>
                   News Feed
                 </a>
               </Link>
             </li>
           </ul>
           <div className="my-account">
-            <Link legacyBehavior href="/account">
+            <Link legacyBehavior href="/account" style={{textDecoration:"none"}}>
               <a>My Account</a>
             </Link>
           </div>
         </nav>
       </div>
-      <div className="header-right">
+      <div className="header-right mr-2">
         <div className="phone">
           <div className="icon">
             <img src="assets/images/icons/phone.svg" alt="" />
@@ -174,20 +207,66 @@ function Header1() {
             <a href="tel:01701111000">+91 7844932404</a>
           </div>
         </div>
-        <div className="wishlist">
+
+        <div className="account-btn" >
           <Link legacyBehavior href="/account">
-            <a>
-              <i className="bi bi-suit-heart" />
-            </a>
+            <a style={{ marginLeft: "-80px" }}>My Account</a>
           </Link>
         </div>
-        <div className="account-btn">
-          <Link legacyBehavior href="/account">
-            <a>My Account</a>
-          </Link>
+        <div className="thumb" onClick={() => (window.location = "/account")}>
+          <center>
+            {image ? (
+              <img
+                src={image}
+                alt=""
+                style={{
+                  height: "50px",
+                  width: "50px",
+                  marginTop: "2px",
+                  marginLeft: "19px",
+                  marginRight: "40px",
+                  boxShadow: "0 4px 8px rgba(228, 6, 6, 0.1)",
+                  borderRadius: "50%", 
+                }}
+              />
+            ) : (
+              <i
+                className="bi bi-person-circle"
+                style={{
+                  height: "50px",
+                  width: "50px",
+                  marginTop: "2px",
+                  paddingTop: "14px",
+                  marginLeft: "19px",
+                  marginRight: "40px",
+                  display: "inline-block",
+                  boxShadow: "0 4px 8px rgba(228, 6, 6, 0.1)",
+                  borderRadius: "50%",
+                }}
+              ></i>
+            )}
+          </center>
         </div>
-        <div className="mobile-menu" onClick={()=>{setMobileheader(true)}}>
-          <a className="cross-btn" onClick={()=>{setMobileheader(false)}}>
+        {fname && typeoff ? (
+          <div className="phn-info" style={{ marginLeft: "-20px" }}>
+            <h2>{typeoff}</h2>
+            <p style={{ marginTop: "-26px" }}>{fname}</p>
+          </div>
+        ) : (
+          ""
+        )}
+        <div
+          className="mobile-menu"
+          onClick={() => {
+            setMobileheader(true);
+          }}
+        >
+          <a
+            className="cross-btn"
+            onClick={() => {
+              setMobileheader(false);
+            }}
+          >
             <span className="cross-top" />
             <span className="cross-middle" />
             <span className="cross-bottom" />
