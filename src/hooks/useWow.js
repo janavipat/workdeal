bacb229980dfa560c384ba59d01d85ff442584b8
janavipat <133246892+janavipat@users.noreply.github.com>
2024-04-22 11:12:52 +0530

    set usewow

--- src/hooks/useWow.js
+++ src/hooks/useWow.js
@@ -1,17 +1,1 @@
-import { useEffect } from "react";
 
-export function useWow() {
-  useEffect(() => {
-    import("wowjs").then((WOW) => {
-      const wow = new WOW.default.WOW({
-        live: false,
-        mobile: false,
-      });
-      wow.init();
-
-      return () => {
-        wow.destroy();
-      };
-    });
-  }, []);
-}
